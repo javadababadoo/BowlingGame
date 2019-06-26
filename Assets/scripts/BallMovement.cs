daa9dbdd98ab812802360c726c7166de209416f8
jaime.zapata <jezg1992@hotmail.com>
2019-06-25 22:39:11 -0500

    first commit

---
+++ Assets/scripts/BallMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+
+    public Rigidbody ball;
+    public int force;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ball.AddForce(0, 0, 140);
+    }
+}
